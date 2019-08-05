61351ee0d6a3d96c8fa15510b4ff1b1c7f1a5279
olavgg <olavgjerde@yahoo.no>
2019-08-05 09:06:29 +0200

    first commit

---
+++ src/main/java/zoomchart/BaseController.java
@@ -0,0 +1,15 @@
+package zoomchart;
+
+import io.micronaut.http.annotation.Controller;
+
+/**
+ * User: olavgjerde
+ * Date: 2019-08-02
+ * Time: 15:13
+ */
+
+@Controller
+public class BaseController {
+
+
+}
